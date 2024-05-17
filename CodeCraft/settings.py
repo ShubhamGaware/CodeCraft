7240938b5eb4dc0ed8dc9c30e62f0fdeb9262ce3
Shubham Gaware <shubhamgaware45@gmail.com>
2024-05-17 15:41:30 +0530

    update

--- CodeCraft/settings.py
+++ CodeCraft/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS=[
-    (BASE_DIR/"static",),
+    (BASE_DIR / "static",),
 ]
 
 MEDIA_ROOT = BASE_DIR /"media"
